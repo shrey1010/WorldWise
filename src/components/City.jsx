7d7dff1bf9c3bc5641b8e9f5c837fe92cc7cb55f
Shrey Shukla <shreyshukla1010@gmail.com>
2024-11-28 17:27:16 +0530

    feat : Button Componennts

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,6 +1,10 @@
 import styles from "./City.module.css";
-// import { useParams } from "react-router-dom";
-import { useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+// import { useSearchParams } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
+import { useEffect } from "react";
+import Spinner from "./Spinner";
+import BackButton from "./BackButton";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -13,23 +17,18 @@ const formatDate = (date) =>
 
 function City() {
 
-  // const {id} = useParams();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
-
-
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
-
+  const {id} = useParams();
+  const {getCity,currentCity,isLoading} = useCities();
   const { cityName, emoji, date, notes } = currentCity;
 
   // return <h1>City</h1> 
+  useEffect(function(){
+    getCity(id)
+
+  }
+  ,[id])
+
+  if (isLoading) return <Spinner/>
 
   return (
     <div className={styles.city}>
@@ -64,7 +63,7 @@ function City() {
       </div>
 
       <div>
-        {/* <ButtonBack /> */}
+        <BackButton />
       </div>
     </div>
   );
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -7,6 +7,7 @@ function CitiesProvider({children}) {
 
     const [cities, setCities] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [currentCity, setCurrentCity] = useState({});
 
     useEffect(function () {
     async function fetchCities() {
@@ -24,8 +25,21 @@ function CitiesProvider({children}) {
     fetchCities();
     }, []);
 
+    async function getCity(id){
+      try {
+        setIsLoading(true);
+        const res = await fetch(`http://localhost:9000/cities/${id}`);
+        const data = await res.json();
+        setCurrentCity(data);
+      } catch {
+        alert("Error fetching cities");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
     return (
-        <CitiesContext.Provider value={{cities, isLoading}}>
+        <CitiesContext.Provider value={{cities, isLoading,currentCity, getCity}}>
             {children}
         </CitiesContext.Provider>
     );
