9d1a5020b8256055376d1c2515056e369e6bb11b
Shrey Shukla <shreyshukla1010@gmail.com>
2024-12-01 16:25:54 +0530

    feat : restricting no authenticated user to use app

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 function User() {
   const { user,logout } = useAuth();
-  const { navigate } = useNavigate();
+  const navigate = useNavigate();
 
   function handleClick() {
     logout();
